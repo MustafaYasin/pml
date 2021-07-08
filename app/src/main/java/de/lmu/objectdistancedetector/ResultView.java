f5b57a1cb55369017caf5755b9146caef9407504
Jannis <jannis.gutleben@campus.lmu.de>
2021-07-08 15:11:26 +0200

    add app to repo

---
+++ app/src/main/java/de/lmu/objectdistancedetector/ResultView.java
@@ -0,0 +1,103 @@
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+package de.lmu.objectdistancedetector;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class ResultView extends View {
+
+    private final static int TEXT_X = 40;
+    private final static int TEXT_Y = 35;
+    private final static int TEXT_WIDTH = 370;
+    private final static int TEXT_HEIGHT = 50;
+
+    private Paint mPaintRectangle;
+    private Paint mPaintText;
+    private ArrayList<de.lmu.objectdistancedetector.Result> mResults;
+
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public ResultView(Context context) {
+        super(context);
+    }
+
+    public ResultView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        mPaintRectangle = new Paint();
+        mPaintRectangle.setColor(Color.YELLOW);
+        mPaintText = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mResults == null) return;
+        for (de.lmu.objectdistancedetector.Result result : mResults) {
+            mPaintRectangle.setStrokeWidth(5);
+            mPaintRectangle.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(result.rect, mPaintRectangle);
+
+            Path mPath = new Path();
+            RectF mRectF = new RectF(result.rect.left, result.rect.top, result.rect.left + TEXT_WIDTH,  result.rect.top + TEXT_HEIGHT);
+            mPath.addRect(mRectF, Path.Direction.CW);
+            mPaintText.setColor(Color.MAGENTA);
+            canvas.drawPath(mPath, mPaintText);
+
+            mPaintText.setColor(Color.WHITE);
+            mPaintText.setStrokeWidth(0);
+            mPaintText.setStyle(Paint.Style.FILL);
+            mPaintText.setTextSize(32);
+
+            double FOCAL = 1142.85;
+            double BOTTLE_W = 7;
+            double distance = (BOTTLE_W * FOCAL) / result.rect.width();
+
+            canvas.drawText(String.format("%s %.2f d:%.1f cm", PrePostProcessor.mClasses[result.classIndex], result.score, distance), result.rect.left + TEXT_X, result.rect.top + TEXT_Y, mPaintText);
+        }
+    }
+
+    public void setResults(ArrayList<de.lmu.objectdistancedetector.Result> results) {
+        mResults = results;
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+        mRatioWidth = width;
+        mRatioHeight = height;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) {
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+}
